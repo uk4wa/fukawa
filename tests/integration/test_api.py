c621941e540c85be1b064f0ea9feb17385f6058f
yukawa08 <python1num@gmail.com>
2026-03-09 22:25:20 +0700

    chore: tests

--- tests/integration/test_api.py
+++ tests/integration/test_api.py
@@ -8,16 +8,16 @@ from sqlalchemy.ext.asyncio import AsyncSession
 async def test_api_organizations_create_success(
     client: AsyncClient, db_session: AsyncSession
 ):
-    params_json = {"name": "okname1"}
-    response = await client.post("/orgs/", json=params_json)
+    name_json = {"name": "okname1"}
+    response = await client.post("/orgs/", json=name_json)
 
     assert response.status_code == 201
     body = response.json()
     assert body["public_id"] is not None
 
     stmt = text("SELECT public_id FROM organizations WHERE name = :name")
-    result = await db_session.execute(stmt, params_json)
+    result = await db_session.execute(stmt, name_json)
 
-    row = result.fetchone()
-    assert row is not None
-    assert str(row.public_id) == body["public_id"]
+    public_id = result.scalar_one()
+    assert public_id is not None
+    assert str(public_id) == body["public_id"]
--- tests/unit/test.py
+++ tests/unit/test.py
@@ -110,3 +110,31 @@ def test_4_calls(mocker: MockerFixture) -> None:
     cicle([1, 2, 3], executor)
 
     mock.assert_has_calls(calls, any_order=False)
+
+# from unittest.mock import Mock
+# from pytest_mock import MockFixture
+
+
+# def test_mock(mocker: MockFixture):
+#     mock_response: Mock = mocker.Mock()
+#     mock_response.json.return_value = {"id": 1}
+
+#     mock_get = mocker.patch("pet.app.ap.requests.get", return_value=mock_response)
+#     result = get_url_request(1)
+
+#     assert result == {"id": 1}
+#     mock_get.assert_called_once_with("https://api.example.com/users/1")
+
+
+# i: int = 0
+
+
+# @pytest.fixture(scope="session")
+# def fixture():
+#     global i
+#     i += 1
+#     db = f"db{i}"
+#     print(f"DB [[[[[[[[[[[INITED--{i}]]]]]]]]]]]")
+#     yield db
+#     db = None
+#     print(f"DB [[[[[[[[[[[CLOSED--{i}]]]]]]]]]]]")
